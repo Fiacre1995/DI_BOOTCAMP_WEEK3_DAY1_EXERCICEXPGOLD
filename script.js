d7163f8e12a41259053686dc3dd394fcd5b7e183
fiacre <armeldion95@gmail.com>
2023-01-11 04:52:02 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let tr1 = document.getElementsByTagName("td")[0];
+tr1.style.backgroundColor ="red";
+
+let tr2 = document.getElementsByTagName("td")[6];
+tr2.style.backgroundColor ="red";
+
+let tr3 = document.getElementsByTagName("td")[12];
+tr3.style.backgroundColor ="red";
+
+let tr4 = document.getElementsByTagName("td")[18];
+tr4.style.backgroundColor ="red";
+
+let tr5 = document.getElementsByTagName("td")[24];
+tr5.style.backgroundColor ="red";
+
+let tr6 = document.getElementsByTagName("td")[20];
+tr6.style.backgroundColor ="red";
+
+let tr7 = document.getElementsByTagName("td")[16];
+tr7.style.backgroundColor ="red";
+
+let tr8 = document.getElementsByTagName("td")[8];
+tr8.style.backgroundColor ="red";
+
+let tr9 = document.getElementsByTagName("td")[4];
+tr9.style.backgroundColor ="red";
